f968b4fb143324c91e0aa2200786abf370f84e36
DanielBichara <danipeter007@hotmail.com>
2021-05-16 17:06:09 -0600

    Guía 6

---
+++ src/main/java/Guia7/Ejer1MainTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Guia7;
+
+import java.util.Scanner;
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ *
+ * @author giama
+ */
+public class Ejer1MainTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner teclado = new Scanner(System.in);
+        String user;
+        String pass, check, hash;
+        
+        System.out.println("CREACIÓN DE UNA CUENTA:");
+        System.out.println("Usuario:");
+        user = teclado.nextLine();
+        System.out.println("Contraseña:");
+        pass = teclado.nextLine();
+        System.out.println("Confirmación:");
+        check = teclado.nextLine();
+        
+        Ejer1 us = new Ejer1(user, null);
+        us.setPassword(us.hash(pass));
+        
+        System.out.println("Contraseña:" + us.password);
+        System.out.println("Confirmación:" + us.hash(check) + "\n");
+        
+        if(BCrypt.checkpw(check, us.getPassword())){
+            System.out.println("Las contraseñas no coinciden.");
+        } else {
+            System.out.println("¡Cuenta creada con éxito!");
+        }
+    }
+    
+}
---
+++ src/main/java/uees/poo/registro3/Bcrypt.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uees.poo.registro3;
+
+import java.util.Scanner;
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ *
+ * @author giama
+ */
+public class Bcrypt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String nombre;
+        String apellidos;
+        Float edad;
+        String departamento;
+        String email;
+        String id_usuario;
+        String password;
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Ingresa el nombre del usuario :");
+        nombre = teclado.nextLine();
+        System.out.println("Ingresa el apellido del usuario :");
+        apellidos = teclado.nextLine();
+        System.out.println("Ingresa la edad :");
+        edad = Float.parseFloat(teclado.nextLine());
+        System.out.println("Ingresa el departamento :");
+        departamento = teclado.nextLine();
+        System.out.println("Ingresa el email :");
+        email = teclado.nextLine();
+        System.out.println("Ingresa el id :");
+        id_usuario = teclado.nextLine();
+        System.out.println("Ingresa el password :");
+        password = teclado.nextLine();
+        
+        String pass_hash;
+        
+        pass_hash = Login.encriptar(password);
+        
+        Login registro;
+        registro = new Login(nombre, apellidos, edad, departamento, email, id_usuario, pass_hash);
+        
+        System.out.println("Los datos de registro son:");
+        System.out.println("Nombre:" + registro.getNombre());
+        System.out.println("Apellios:" + registro.getApellidos());
+        System.out.println("Edad:" + registro.getEdad());
+        System.out.println("Departamento:" + registro.getDepartamento());
+        System.out.println("Email:" + registro.getEmail());
+        System.out.println("Id:" + registro.getId_usuario());
+        System.out.println("Password:" + registro.getPassword());
+        
+        String password_check;
+        System.out.println("Ingresa el password a verificar :");
+        password_check = teclado.nextLine();
+        
+        if (BCrypt.checkpw(password_check, registro.getPassword())){
+            
+            System.out.println("El password es correcto");
+            
+        } else {
+            
+            System.out.println("El password es incorrecto");
+            
+        }
+        
+    }
+    
+}
